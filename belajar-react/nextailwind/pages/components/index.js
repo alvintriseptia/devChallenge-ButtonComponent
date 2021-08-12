319e23155df01b6cdcc17badd40c1a8501e8dae1
Alvin Triseptia <72953335+alvintriseptia@users.noreply.github.com>
2021-08-12 17:48:50 +0700

    Delete belajar-react/nextailwind directory

--- belajar-react/nextailwind/pages/components/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Navbar} from './Navbar/Navbar'
-export {default as FeaturedPost} from './Post/FeaturedPost/FeaturedPost'
-export {default as CardPost} from './Post/CardPost/CardPost'
-export {default as posts} from './Post/CardPost/posts'
-export {default as Footer} from './Footer/Footer'
