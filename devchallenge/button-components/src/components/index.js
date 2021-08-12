0d17a79c4b3758d3c55aa91b61e74f8d78a09bdc
Alvin Triseptia <72953335+alvintriseptia@users.noreply.github.com>
2021-08-12 17:48:39 +0700

    Delete devchallenge/button-components directory

--- devchallenge/button-components/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as Documentation } from "./Documentation/Documentation";
-export { default as User } from "./User/User";
-export { default as Footer } from "./Footer/Footer";
